5c3bb9059f8b22930bf40c27d00a645193b59493
patrick bragg <Pbbragg1@boe.jack.k12.wv.us>
2025-02-10 20:12:04 -0500

    added a model file, connected model file to new project page, havent connected to index page

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
-from flask import Flask, render_template, url_for, request
-
-
-app = Flask(__name__)
+from flask import Flask, render_template, url_for, request, redirect
+from models import db, Projects, app
 
 
 @app.route('/')
@@ -10,7 +8,11 @@ def index():
 
 @app.route('/projects/new', methods=['GET', 'POST'])
 def new_project():
-    print(request.form)
+    if request.form:
+        new_project_ = Projects(title=request.form['title'], date=request.form['date'], description=request.form['desc'], skills=request.form['skills'], github=['github'])
+        db.session.add(new_project_)
+        db.session.commit(new_project_)
+        return redirect(url_for('index'))
     return render_template('projectform.html')
 
 @app.route('/{id}')
@@ -29,4 +31,6 @@ def delete():
 
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True, port=8000, host='127.0.0.1')
